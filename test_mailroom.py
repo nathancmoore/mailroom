ae4b87d48da8e55c880469f1c063eed13c85fbce
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-20 14:18:48 -0700

    test to check names are being returned

--- test_mailroom.py
+++ test_mailroom.py
@@ -1,1 +1,18 @@
 """Various test for our mailroom.py file."""
+
+
+import pytest
+
+def test_generated_donor_names():
+    """Test that random names are generated."""
+    from mailroom import generate_donor_names
+    donor_names = generate_donor_names()
+    assert donor_names
+
+
+def test_names_are_strings():
+    """Test that the names are all strings."""
+    from mailroom import generate_donor_names
+    donor_names = generate_donor_names()
+    for name in donor_names:
+        assert type(name) == str
