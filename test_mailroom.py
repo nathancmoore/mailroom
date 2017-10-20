9d5a9aec465128da5e596a60c3a1a6810d05a152
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-20 15:06:28 -0700

    testing dictionary, passing

--- test_mailroom.py
+++ test_mailroom.py
@@ -32,3 +32,19 @@ def test_multiple_donation_amounts():
     donations = generate_donations(['bill', 'bob', 'joe'])
     for i in range(len(donations)):
         assert donations[i] != 0
+
+
+def test_dictionary():
+    """Test that that dictionary is not empty."""
+    import mailroom
+    mailroom.build_dictionary()
+    assert mailroom.donors.keys() != 0
+
+
+def test_dictionary_values():
+    """Test there are values attached to keys in the dictionary."""
+    import mailroom
+    mailroom.build_dictionary()
+    for key in mailroom.donors:
+        value = mailroom.donors.get(key)
+        assert value != 0
