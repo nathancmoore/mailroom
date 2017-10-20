e677945326be7b6cf211bcb8f15694720712c4bc
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-20 16:00:04 -0700

    worked on setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name="mailroom",
     description="Command-line script used to track donations and \
     write thank you notes.",
-    license="MIT",
-    py_modules=['function_names'],
+    py_modules=['mailroom'],
     package_dir={'mailroom'},
     install_requires=[],
-    extras_requires={'test': ['pytest, pytest-watch', 'tox']},
+    extras_require={'test': ['pytest', 'pytest-watch', 'tox']},
     )
